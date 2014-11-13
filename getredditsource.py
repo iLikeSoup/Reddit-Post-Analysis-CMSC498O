e9c06d57de782314999a66b2691f507af3cdd3d4
iLikeSoup <bneumaie@terpmail.umd.edu>
2014-11-13 15:08:43 -0500

    pushing new datasets

--- getredditsource.py
+++ getredditsource.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python
 from lxml import html
 import urllib2
+import sys
 
 # Pull Reddit "front-page" source code
-page = urllib2.urlopen("http://www.reddit.com?limit=25").read()
+page = urllib2.urlopen("http://www.reddit.com?limit={}".format(sys.argv[1])).read()
 
 print page
